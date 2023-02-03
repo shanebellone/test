cb982c7b370a8f6e265cd3ce00af7ca9b6ad0915
Shane Bellone <40317618+shanebellone@users.noreply.github.com>
2023-02-03 10:22:44 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, send_from_directory
+from jinja2 import TemplateNotFound
+
+from utilities.sitemap import generate_sitemap
+
+app = Flask(__name__)
+
+
+@app.route('/', defaults={'page': 'index'})
+@app.route('/<page>/')
+def route_url(page, api=None):
+    match page:
+        case "sitemap.txt":
+            generate_sitemap()
+            return send_from_directory('static', "sitemap.txt")
+    try:
+        return render_template('/posts/{page}.html'.format(page=page))
+    except TemplateNotFound:
+        return "404"
+
+
+if __name__ == '__main__':
+
+    app.run()
---
+++ utilities/sitemap.py
@@ -0,0 +1,11 @@
+import os
+
+
+def generate_sitemap():
+    domain = "https://shanebellone.com/"
+    path = "../templates/posts/"
+    urls = [(domain + file[:-5]) for file in os.listdir(path)]
+    with open(r'static/sitemap.txt', 'w') as sitemap:
+        for url in urls:
+            sitemap.write("%s\n" % url)
+    sitemap.close()
